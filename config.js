85f5757ce06b0faa6a315c1875f7051bd63e4553
jimenezaxel565-svg <jimenezaxel565@gmail.com>
2026-04-04 10:59:29 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    // Velocidad base (Palabras Por Minuto)
+    // 160 = Tranquilo | 190 = Normal/Fluido | 220 = Rápido
+    ppm: 150, 
+
+    // Espacio entre párrafos (en porcentaje de altura de pantalla)
+    espaciadoParrafos: "10vh",
+
+    // Tamaño de letra principal
+    fontSizePrincipal: "32px",
+
+    // Punto de inicio (qué tan abajo empieza el texto)
+    // Menos valor = empieza a subir más rápido
+    paddingInicial: "50vh",
+
+    // Sensibilidad del control táctil (cuánto aumenta/baja con cada toque)
+    sensibilidadPaso: 0.2
+};
